c51a26d2d01a6389479fa429b34969d3d8f07359
Clarence ANTONMERYL <clarence.antonmeryl@gmail.com>
2022-07-01 19:48:46 +1000

    Add fishing.py, update DataGenerator.py

--- DataGenerator.py
+++ DataGenerator.py
@@ -21,6 +21,16 @@ class DataGenerator:
         B_n_plus_one = (r / A_E) * A_n * B_n
         return B_n_plus_one
 
+    @staticmethod
+    def squid_population_fished(A_n, B_n, R, A_E, C, k, P, A):
+        A_n_plus_one = R * A_n - ((R - 1) / A_E) * (A_n ** 2) - (C * A_n * B_n) - ((k * P) / A)
+        return A_n_plus_one
+
+    @staticmethod
+    def seal_population_fished(A_n, B_n, A_E, r, k, P, A):
+        B_n_plus_one = (r / A_E) * A_n * B_n - ((k * P) / A)
+        return B_n_plus_one
+
     def get_population_lists(self, a_n, b_n, R=3.0, A_E=100.0, C=0.5, r=2.0):
         squid_population_list = []
         seal_population_list = []
@@ -45,6 +55,119 @@ class DataGenerator:
                     B_n=seal_population_list[-1],
                     A_E=A_E,
                     r=r
+                )
+            )
+
+        return squid_population_list, seal_population_list
+
+    def get_population_lists_squid_fished(self, a_n, b_n, k, P, A, R=3.0, A_E=100.0, C=0.5, r=2.0):
+        squid_population_list = []
+        seal_population_list = []
+
+        squid_population_list.append(a_n)
+        seal_population_list.append(b_n)
+
+        for _ in range(len(X) - 1):
+            squid_population_list.append(
+                self.squid_population_fished(
+                    A_n=squid_population_list[-1],
+                    B_n=seal_population_list[-1],
+                    R=R,
+                    A_E=A_E,
+                    C=C,
+                    k=k,
+                    P=P,
+                    A=A
+                )
+            )
+
+            seal_population_list.append(
+                self.seal_population(
+                    A_n=squid_population_list[-2],
+                    B_n=seal_population_list[-1],
+                    A_E=A_E,
+                    r=r
+                )
+            )
+
+        return squid_population_list, seal_population_list
+
+    def get_population_lists_seal_fished(self, a_n, b_n, k, P, A, R=3.0, A_E=100.0, C=0.5, r=2.0):
+        squid_population_list = []
+        seal_population_list = []
+
+        squid_population_list.append(a_n)
+        seal_population_list.append(b_n)
+
+        for _ in range(len(X) - 1):
+            squid_population_list.append(
+                self.squid_population(
+                    A_n=squid_population_list[-1],
+                    B_n=seal_population_list[-1],
+                    R=R,
+                    A_E=A_E,
+                    C=C
+                )
+            )
+
+            seal_population_list.append(
+                self.seal_population_fished(
+                    A_n=squid_population_list[-2],
+                    B_n=seal_population_list[-1],
+                    A_E=A_E,
+                    r=r,
+                    k=k,
+                    P=P,
+                    A=A
+                )
+            )
+
+        return squid_population_list, seal_population_list
+
+    def get_population_lists_both_fished(
+            self,
+            a_n,
+            b_n,
+            k_squid,
+            P_squid,
+            A_squid,
+            k_seal,
+            P_seal,
+            A_seal,
+            R=3.0,
+            A_E=100.0,
+            C=0.5,
+            r=2.0
+    ):
+        squid_population_list = []
+        seal_population_list = []
+
+        squid_population_list.append(a_n)
+        seal_population_list.append(b_n)
+
+        for _ in range(len(X) - 1):
+            squid_population_list.append(
+                self.squid_population_fished(
+                    A_n=squid_population_list[-1],
+                    B_n=seal_population_list[-1],
+                    R=R,
+                    A_E=A_E,
+                    C=C,
+                    k=k_squid,
+                    P=P_squid,
+                    A=A_squid
+                )
+            )
+
+            seal_population_list.append(
+                self.seal_population_fished(
+                    A_n=squid_population_list[-2],
+                    B_n=seal_population_list[-1],
+                    A_E=A_E,
+                    r=r,
+                    k=k_seal,
+                    P=P_seal,
+                    A=A_seal
                 )
             )
 
---
+++ fishing.py
@@ -0,0 +1,144 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from DataGenerator import DataGenerator
+
+data_generator = DataGenerator()
+
+MEASUREMENTS = 500
+
+X = np.linspace(0, MEASUREMENTS, MEASUREMENTS)
+
+
+def config_graph():
+    plt.figure(figsize=(24, 14))
+    plt.xlim(0, MEASUREMENTS)
+    plt.xlabel("Time (n)")
+    plt.ylabel("Population Density")
+
+
+def plot_data_squid_fishing(k=25000, P=0.9, A=3000):
+    squid_population_list_fishing, seal_population_list_squid_fishing = data_generator.get_population_lists_squid_fished(
+        a_n=50,
+        b_n=0.2,
+        k=k,
+        P=P,
+        A=A
+    )
+
+    squid_population_list, seal_population_list = data_generator.get_population_lists(a_n=50, b_n=0.2)
+
+    config_graph()
+
+    ax = plt.gca()
+    ax.set_yticks(range(0, 96, 5))
+
+    plt.title(f"Population Density of Squid Over Time With and Without Fishing ({MEASUREMENTS} measurements)")
+    plt.plot(X, squid_population_list_fishing, color="red", alpha=0.6)
+    plt.plot(X, squid_population_list, color="blue", alpha=0.6)
+
+    plt.show()
+
+    config_graph()
+
+    plt.title(f"Population Density of Seal Over Time With and Without Squid Fishing ({MEASUREMENTS} measurements)")
+    plt.plot(X, seal_population_list_squid_fishing, color="red", alpha=0.6)
+    plt.plot(X, seal_population_list, color="blue", alpha=0.6)
+
+    plt.show()
+
+    config_graph()
+
+    plt.title(f"Environment With Squid Fishing ({MEASUREMENTS} measurements)")
+    plt.plot(X, seal_population_list_squid_fishing, color="red", alpha=0.6)
+    plt.plot(X, squid_population_list_fishing, color="blue", alpha=0.6)
+
+    plt.show()
+
+
+def plot_data_seal_fishing(k=198, P=0.9, A=3000):
+    squid_population_list_seal_fishing, seal_population_list_fishing = data_generator.get_population_lists_seal_fished(
+        a_n=50,
+        b_n=0.2,
+        k=k,
+        P=P,
+        A=A
+    )
+
+    squid_population_list, seal_population_list = data_generator.get_population_lists(a_n=50, b_n=0.2)
+
+    config_graph()
+
+    ax = plt.gca()
+    ax.set_yticks(range(0, 101, 5))
+
+    plt.title(f"Population Density of Squid Over Time With and Without Seal Fishing ({MEASUREMENTS} measurements)")
+    plt.plot(X, squid_population_list_seal_fishing, color="red", alpha=0.6)
+    plt.plot(X, squid_population_list, color="blue", alpha=0.6)
+
+    plt.show()
+
+    config_graph()
+
+    plt.title(f"Population Density of Seal Over Time With and Without Fishing ({MEASUREMENTS} measurements)")
+    plt.plot(X, seal_population_list_fishing, color="red", alpha=0.6)
+    plt.plot(X, seal_population_list, color="blue", alpha=0.6)
+
+    plt.show()
+
+    config_graph()
+
+    plt.title(f"Environment With Seal Fishing ({MEASUREMENTS} measurements)")
+    plt.plot(X, seal_population_list_fishing, color="red", alpha=0.6)
+    plt.plot(X, squid_population_list_seal_fishing, color="blue", alpha=0.6)
+
+    plt.show()
+
+
+def plot_data_both_fishing(k_squid=25000, P_squid=0.9, A_squid=3000, k_seal=198, P_seal=0.9, A_seal=3000):
+    squid_population_list_both_fishing, seal_population_both_fishing = data_generator.get_population_lists_both_fished(
+        a_n=50,
+        b_n=0.2,
+        k_squid=k_squid,
+        P_squid=P_squid,
+        A_squid=A_squid,
+        k_seal=k_seal,
+        P_seal=P_seal,
+        A_seal=A_seal
+    )
+
+    squid_population_list, seal_population_list = data_generator.get_population_lists(a_n=50, b_n=0.2)
+
+    config_graph()
+
+    ax = plt.gca()
+    ax.set_yticks(range(0, 96, 5))
+
+    plt.title(
+        f"Population Density of Squid Over Time With and Without Both Species Fished ({MEASUREMENTS} measurements)")
+    plt.plot(X, squid_population_list_both_fishing, color="red", alpha=0.6)
+    plt.plot(X, squid_population_list, color="blue", alpha=0.6)
+
+    plt.show()
+
+    config_graph()
+
+    plt.title(
+        f"Population Density of Seal Over Time With and Without Both Species Fished ({MEASUREMENTS} measurements)")
+    plt.plot(X, seal_population_both_fishing, color="red", alpha=0.6)
+    plt.plot(X, seal_population_list, color="blue", alpha=0.6)
+
+    plt.show()
+
+    config_graph()
+
+    plt.title(f"Environment With Both Species Fished ({MEASUREMENTS} measurements)")
+    plt.plot(X, seal_population_both_fishing, color="red", alpha=0.6)
+    plt.plot(X, squid_population_list_both_fishing, color="blue", alpha=0.6)
+
+    plt.show()
+
+
+plot_data_squid_fishing()
+plot_data_seal_fishing()
+plot_data_both_fishing()
